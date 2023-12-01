bf722fc596fb77a8e1ead3c96d13fe061b8d367d
unknown <you@example.com>
2023-12-02 00:31:03 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,17 @@
+var boxgift = document.querySelector('.box-gift')
+
+var Close = document.querySelector('.fa-xmark')
+
+var boxContent = document.querySelector('.box-content')
+
+var content =document.querySelector('.content')
+
+boxgift.onclick = function(){
+    boxgift.classList.toggle('active')
+}
+content.onclick = function(){
+    boxContent.classList.add('active')
+}
+Close.onclick = function(){
+    boxContent.classList.remove('active')
+}
